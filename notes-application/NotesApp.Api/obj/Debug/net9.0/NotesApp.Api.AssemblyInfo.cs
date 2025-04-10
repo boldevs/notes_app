a5eb3f6703050aa36efe8782e7a5836df1f63440
boldevs <yonviibol2222@gmail.com>
2025-04-10 09:51:42 +0700

    Add node version in package.json

--- notes-application/NotesApp.Api/obj/Debug/net9.0/NotesApp.Api.AssemblyInfo.cs
+++ notes-application/NotesApp.Api/obj/Debug/net9.0/NotesApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb42edcc68ebbe795647b1e103950aea73d31b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cada0cdf61f72d2a4cb56815efc219ad49e47217")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
